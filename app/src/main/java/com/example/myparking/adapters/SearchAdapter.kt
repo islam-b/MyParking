f6260af0c5035d40755f9e0bf4d838abc832a1ca
amira96 <fa_aiouez@esi.dz>
2020-06-29 18:47:12 +0100

    search bug fixed

--- app/src/main/java/com/example/myparking/adapters/SearchAdapter.kt
+++ app/src/main/java/com/example/myparking/adapters/SearchAdapter.kt
@@ -1,5 +1,6 @@
 package com.example.myparking.adapters
 
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -11,11 +12,13 @@ import com.example.myparking.R
 import com.example.myparking.models.SearchResult
 import org.w3c.dom.Text
 
-class SearchAdapter(val suggestions: ArrayList<SearchResult>, val listener: OnSearchListener) : RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
-    
+class SearchAdapter(val suggestions: ArrayList<SearchResult>, val listener: OnSearchListener) :
+    RecyclerView.Adapter<SearchAdapter.SearchViewHolder>() {
+
     override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): SearchViewHolder {
-        val view = LayoutInflater.from(parent.context).inflate(R.layout.search_item_layout,parent,false)
-        return SearchViewHolder(view,listener)
+        val view =
+            LayoutInflater.from(parent.context).inflate(R.layout.search_item_layout, parent, false)
+        return SearchViewHolder(view, listener)
     }
 
     override fun getItemCount(): Int {
@@ -26,14 +29,34 @@ class SearchAdapter(val suggestions: ArrayList<SearchResult>, val listener: OnSe
         val suggestion = suggestions[position]
         holder.description.text = suggestion.title
         holder.label.text = suggestion.vicinity
-        holder.searchRes = suggestion
+        Log.d("searchinngggg", suggestion?.toString()!!)
+        if (suggestion.title === "") {
+            val newSug = SearchResult(
+                title = suggestion.vicinity,
+                vicinity = suggestion.vicinity,
+                highlightedTitle = suggestion.highlightedTitle,
+                href = suggestion.href,
+                type = suggestion.type,
+                resultType = suggestion.resultType,
+                highlightedVicinity = suggestion.highlightedVicinity,
+                position = suggestion.position,
+                category = suggestion.category,
+                categoryTitle = suggestion.categoryTitle,
+                id = suggestion.categoryTitle,
+                distance = suggestion.distance
+            )
+            holder.searchRes = newSug
+        } else
+            holder.searchRes = suggestion
     }
 
-    inner class SearchViewHolder(val view: View, val listener: OnSearchListener): RecyclerView.ViewHolder(view),
+    inner class SearchViewHolder(val view: View, val listener: OnSearchListener) :
+        RecyclerView.ViewHolder(view),
         View.OnClickListener {
         init {
             view.setOnClickListener(this)
         }
+
         lateinit var searchRes: SearchResult
         val label = view.findViewById<TextView>(R.id.label)
         val description = view.findViewById<TextView>(R.id.description)
@@ -44,6 +67,7 @@ class SearchAdapter(val suggestions: ArrayList<SearchResult>, val listener: OnSe
     }
 
 }
+
 interface OnSearchListener {
     fun onSearchClick(searchResult: SearchResult)
 }
--- app/src/main/java/com/example/myparking/fragements/SearchDialogFragment.kt
+++ app/src/main/java/com/example/myparking/fragements/SearchDialogFragment.kt
@@ -34,8 +34,7 @@ import retrofit2.Call
 import retrofit2.Callback
 import retrofit2.Response
 
-class SearchDialogFragment: DialogFragment(), OnSearchListener {
-
+class SearchDialogFragment : DialogFragment(), OnSearchListener {
 
 
     val TAG1 = "SearchDialogFragment"
@@ -44,9 +43,11 @@ class SearchDialogFragment: DialogFragment(), OnSearchListener {
 
     override fun onSearchClick(searchResult: SearchResult) {
         dismiss()
-        val args = bundleOf("viewType" to MAP_VIEW, "actionType" to MapAction.SEARCH_ACTION,
-            "data" to searchResult)
-        val navController = Navigation.findNavController(activity!!,R.id.my_nav_host_fragment)
+        val args = bundleOf(
+            "viewType" to MAP_VIEW, "actionType" to MapAction.SEARCH_ACTION,
+            "data" to searchResult
+        )
+        val navController = Navigation.findNavController(activity!!, R.id.my_nav_host_fragment)
         navController.navigate(R.id.action_global_mainActivity2, args)
 
     }
@@ -58,9 +59,10 @@ class SearchDialogFragment: DialogFragment(), OnSearchListener {
         savedInstanceState: Bundle?
     ): View? {
         super.onCreateView(inflater, container, savedInstanceState)
-        val view = inflater.inflate(R.layout.search_dialog_layout,container,false)
+        val view = inflater.inflate(R.layout.search_dialog_layout, container, false)
         return view
     }
+
     private var toolbar: Toolbar? = null
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
@@ -71,16 +73,16 @@ class SearchDialogFragment: DialogFragment(), OnSearchListener {
             it.setNavigationOnClickListener { v -> dismiss() }
         }
 
-        var searchView=view.findViewById<SearchView>(R.id.search_view)
+        var searchView = view.findViewById<SearchView>(R.id.search_view)
         val searchViewIcon =
             searchView.findViewById(androidx.appcompat.R.id.search_mag_icon) as ImageView
         val linearLayoutSearchView = searchViewIcon.parent as ViewGroup
         linearLayoutSearchView.removeView(searchViewIcon)
         recyclerView = view.findViewById<RecyclerView>(R.id.search_list)
-        recyclerView.layoutManager = LinearLayoutManager(context,RecyclerView.VERTICAL,false)
+        recyclerView.layoutManager = LinearLayoutManager(context, RecyclerView.VERTICAL, false)
         loading.visibility = GONE
         val dialog = this
-        searchView.setOnQueryTextListener(object: SearchView.OnQueryTextListener,
+        searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener,
             Callback<SearchModel> {
 
 
@@ -91,29 +93,32 @@ class SearchDialogFragment: DialogFragment(), OnSearchListener {
             override fun onQueryTextChange(newText: String?): Boolean {
 
                 recyclerView.adapter = null
-                if(newText!==null && newText.length>0) {
+                if (newText !== null && newText.length > 0) {
                     service.searchPlaces(newText).enqueue(this)
                     loading.visibility = VISIBLE
                 }
 
                 return false
             }
+
             override fun onFailure(call: Call<SearchModel>, t: Throwable) {
-                Log.d("errorSearch",t.printStackTrace().toString())
+                Log.d("errorSearch", t.printStackTrace().toString())
                 loading?.visibility = GONE
             }
 
             override fun onResponse(call: Call<SearchModel>, response: Response<SearchModel>) {
                 loading?.visibility = GONE
-                val list = response.body()?.results!!
+                val list =
+                    ArrayList(response.body()?.results?.filter { item -> item.position !== null }!!)
                 list.let {
-                    recyclerView.adapter = SearchAdapter(it,dialog)
+                    recyclerView.adapter = SearchAdapter(it, dialog)
                 }
             }
 
         })
 
     }
+
     override fun onStart() {
         super.onStart()
         val dialog = dialog
@@ -122,10 +127,12 @@ class SearchDialogFragment: DialogFragment(), OnSearchListener {
             val height = ViewGroup.LayoutParams.MATCH_PARENT
             dialog.window?.setLayout(width, height)
             dialog.window!!.addFlags(WindowManager.LayoutParams.FLAG_DRAWS_SYSTEM_BAR_BACKGROUNDS)
-            dialog.window!!.statusBarColor = ContextCompat.getColor(context!!,R.color.colorPrimaryDark)
-            dialog.window!!.decorView.systemUiVisibility  = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+            dialog.window!!.statusBarColor =
+                ContextCompat.getColor(context!!, R.color.colorPrimaryDark)
+            dialog.window!!.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_LAYOUT_STABLE
         }
     }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         service = InjectorUtils.provideParkingService()
